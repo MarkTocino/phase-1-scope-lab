e09dad44ab1be5d308f86646308b7de96b631a90
MarkTocino <Mark_tocino@yahoo.com>
2023-06-14 13:14:46 -0400

    Completed Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastBestCustomer = 'sarah'
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase() 
+};
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+};
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer() {
+    leastBestCustomer = 'joe'
+}
